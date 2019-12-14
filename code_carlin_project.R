d79224c49936c38ae29bdad1665ed3da571e88b0
PCarlin <pcarlin2014@gmail.com>
2019-12-14 15:29:04 -0500

    second_commit_revised_code_fake_data

--- code_carlin_project.R
+++ code_carlin_project.R
@@ -40,3 +40,96 @@ bacon = function(outcome,post,treat,post_treat,place,time, cluster, st_errors='s
   return(summary(lmr))
 }
 
+treatment_timing = function(place_input, place, time, post){
+  return(min(subset(time, place==place_input & post==1)))
+}
+
+adjusted_treatment = function(place_input,time_input, place, time){
+  grand_mean_treatment = mean(post_treat)
+  place_mean = mean(subset(post_treat,place == place_input))
+  time_mean = mean(subset(post_treat,time == time_input))
+  adjusted_treatment = subset((post_treat-grand_mean_treatment) - (place_mean-grand_mean_treatment)
+                              - (time_mean-grand_mean_treatment),place==place_input & time==time_input)
+  return(adjusted_treatment)
+}
+
+proportions = function(place,outcome,time){
+  treatment_timing = sapply(place,treatment_timing,place,time,post)
+  treatment_timing[treatment_timing == "Inf"] = 3000
+  index_treatment = unique(treatment_timing)
+  
+  early_adopters = subset(place,treatment_timing==index_treatment[1])
+  n_early = length(early_adopters)
+  early_adopters = unique(subset(place,treatment_timing==index_treatment[1]))
+  late_adopters = subset(place,treatment_timing==index_treatment[2])
+  n_late = length(late_adopters)                       
+  late_adopters = unique(subset(place,treatment_timing==index_treatment[2]))
+  never_adopters = subset(place,treatment_timing==index_treatment[3])
+  n_never = length(never_adopters)
+  never_adopters = unique(subset(place,treatment_timing==index_treatment[3]))
+  n_total = length(outcome)
+  n_early = n_early/n_total
+  n_late = n_late/n_total
+  n_never = n_never/n_total
+  
+  time_total = length(unique(time))
+  place_total = length(unique(place))
+  df = data.frame(place,time)
+  
+  adj_treat = rep(NA,length(df[[1]]))
+  for (i in 1:length(df[[1]])){
+    adj_treat[i] = adjusted_treatment(df[i,1],df[i,2], place, time) 
+  }
+  
+  time_share_early = (max(time)-index_treatment[1]+1)/((max(time)-min(time))+1)
+  time_share_late = (max(time)-index_treatment[2]+1)/((max(time)-min(time))+1)
+  
+  return(c(n_never,n_early,n_late,time_share_early,time_share_late,time_total,place_total,adj_treat))
+}
+
+weights_bacon = function(n_never,n_early,n_late,time_share_early,time_share_late,time_total,place_total,adj_treat){
+  weight_sku = (n_early*n_never*time_share_early*(1-time_share_early))/
+    ((1/(time_total*place_total))*sum(adj_treat^2))
+  weight_slu = (n_late*n_never*time_share_late*(1-time_share_late))/
+    ((1/(time_total*place_total))*sum(adj_treat^2))
+  weight_mukl = (1-time_share_early)/(1-(time_share_early-time_share_late))
+  weight_skl = (n_early*n_late*(time_share_early-time_share_late)*(1-(time_share_early-time_share_late)))/
+    ((1/(time_total*place_total))*sum(adj_treat^2))
+  weight_late_cntl = weight_skl*weight_mukl
+  weight_early_cntl = -(weight_skl*weight_mukl)
+  return(c(weight_sku,weight_slu,weight_late_cntl,weight_early_cntl))
+}
+
+dd = function(outcome,place,time,post_treat){
+  treatment_timing = sapply(place,treatment_timing)
+  treatment_timing[treatment_timing == "Inf"] = 3000
+  index_treatment = unique(treatment_timing)
+  
+  never_adopters_pre_early = mean(subset(outcome,year < index_treatment[1] & treatment_timing == index_treatment[3]))
+  never_adopters_post_early = mean(subset(outcome,year >= index_treatment[1] & treatment_timing == index_treatment[3]))
+  never_adopters_pre_late = mean(subset(outcome,year < index_treatment[2] & treatment_timing == index_treatment[3]))
+  never_adopters_post_late = mean(subset(outcome,year >= index_treatment[2] & treatment_timing == index_treatment[3]))
+  
+  early_adopters_pre_early = mean(subset(outcome,year < index_treatment[1] & treatment_timing == index_treatment[1]))
+  early_adopters_post_early = mean(subset(outcome,year >= index_treatment[1] & treatment_timing == index_treatment[1]))
+  late_adopters_pre_late = mean(subset(outcome,year < index_treatment[2] & treatment_timing == index_treatment[2]))
+  late_adopters_post_late= mean(subset(outcome,year >= index_treatment[2] & treatment_timing == index_treatment[2]))
+  
+  
+  early_cntl_early_pre = mean(subset(outcome,year >= index_treatment[1] & year < index_treatment[2] & treatment_timing == index_treatment[1]))
+  early_cntl_early_post = mean(subset(outcome,year >= index_treatment[2] & treatment_timing == index_treatment[1]))
+  early_cntl_late_pre = mean(subset(outcome,year >= index_treatment[1] & year < index_treatment[2] & treatment_timing == index_treatment[2]))
+  early_cntl_late_post = mean(subset(outcome,year >= index_treatment[2] & treatment_timing == index_treatment[2]))
+  
+  late_cntl_early_pre = mean(subset(outcome,year < index_treatment[1] & treatment_timing == index_treatment[1]))
+  late_cntl_early_post = mean(subset(outcome,year >= index_treatment[1] & year < index_treatment[2] & treatment_timing == index_treatment[1]))
+  late_cntl_late_pre = mean(subset(outcome,year < index_treatment[1] & treatment_timing == index_treatment[2]))
+  late_cntl_late_post = mean(subset(outcome,year >= index_treatment[1] & year < index_treatment[2] & treatment_timing == index_treatment[2]))
+  
+  beta_early_never = (early_adopters_post_early - early_adopters_pre_early) - (never_adopters_post_early - never_adopters_pre_early)
+  beta_late_never = (late_adopters_post_late - late_adopters_pre_late) - (never_adopters_post_late - never_adopters_pre_late)
+  beta_early_cntl = (early_cntl_late_post - early_cntl_late_pre) - (early_cntl_early_post - early_cntl_early_pre)
+  beta_late_cntl = (late_cntl_early_post - late_cntl_early_pre) - (late_cntl_late_post - late_cntl_late_pre)
+  
+  return(c(beta_early_never,beta_late_never,beta_early_cntl,beta_late_cntl))
+}
