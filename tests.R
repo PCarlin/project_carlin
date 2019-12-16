4854ddc22398fb08b19b20624b4285f58f139632
PCarlin <pcarlin2014@gmail.com>
2019-12-15 20:33:35 -0500

    updated_test_fn

--- tests.R
+++ tests.R
@@ -7,6 +7,9 @@
 ###################################################################################
 
 setwd("C:/Users/Pat C/Box/_My Documents/Active/Class/Statistical Computing/Project")
+
+source('code_carlin_project.R')
+
 if(!require(testthat)){
   install.packages("testthat")
   library(testthat)
@@ -15,10 +18,8 @@ if(!require(testthat)){
 test_that("Simple Functions Return Correct Values with Provided Data", {
   expect_equal(treatment_timing("AL",c("AL","AL","AL","AL","AL"),c(1901,1902,1903,1904,1905),c(0,0,1,1,1)),1903)
   expect_equal(adjusted_treatment(c("AL","AL","AL","AL","AL"),c(1901,1902,1903,1904,1905), "AL", 1903,c(0,0,1,1,1)),0)
-  expect_equal(weights_bacon(.25,.25,.5,.33,.33,15,5,c(0.52055311,0.28368973,0.27984078,0.09557787,0.05240121,0.22474658,
-                                                       0.56071443,0.66681220,0.23417615,0.81725573,0.62716355,0.01050599,
-                                                       0.73215510,0.28667766,0.02117444)),
-                                                      c(0.3470803,0.6941605,0,0))
+  expect_equal(weights_bacon(.25,.25,.5,.33,.33,15,5,c(0.5,0.3,0.3,0.1,0.1,0.2,0.6,0.7,0.2,0.8,0.6,0.01,0.7,0.3,0.02)),
+                                                      c(0.350078111805438,0.700156223610876,0,0))
 })
 
 test_that("Functions Return Correct Type of Data", {
